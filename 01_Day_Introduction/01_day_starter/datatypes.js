b8df643c3984354e26070bb5756b897c903d6439
nhung1291 <hongnhungnguyen1291@gmail.com>
2023-03-09 18:18:45 -0500

    feat/exercise-day1

---
+++ 01_Day_Introduction/01_day_starter/datatypes.js
@@ -0,0 +1,5 @@
+console.log(typeof 5);
+console.log(typeof 'Hi');
+console.log(typeof undefined);
+console.log(typeof true);
+console.log(typeof null);
---
+++ 01_Day_Introduction/ex1.js
@@ -0,0 +1,11 @@
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+/**Comments can make code readable,
+ * easy to reuse and informative
+ */
+let name = 'Nhung';
+let bool = true;
+let variable = null;
+let age;
+
+
